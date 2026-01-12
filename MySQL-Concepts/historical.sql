07e1b10c1c1948c11956a9449d768bd94b722510
Frostbite621 <97651468+Frostbite621@users.noreply.github.com>
2026-01-11 23:00:08 -0600

    Update historical.sql

--- MySQL-Concepts/historical.sql
+++ MySQL-Concepts/historical.sql
@@ -1,1 +1,35 @@
+--Create historical schema to make a historical entity
+--Demonstrate knowledge of creating schema
+--Demonstrate knowledge of linking historical entitys
 
+create schema historical;
+
+drop table historical.supervisor;
+drop table historical.employee;
+drop table employee;
+
+create table Employee(
+empid int primary key,
+name varchar(20),
+salary varchar(10),
+title varchar(20),
+supervisorid int not null);
+
+alter table Employee add constraint employee_supervisoryid_fk foreign key (supervisorid) references Employee(empid);
+
+create table historical.employee(
+empid int primary key,
+name varchar(20),
+salary varchar(10),
+title varchar(20));
+
+create table historical.supervisor(
+empid int not null,
+supervisorid int not null,
+startdate date,
+enddate date);
+
+
+alter table historical.supervisor add constraint historicalsupervisor_fk foreign key (supervisorid) references historical.employee(empid);
+alter table historical.supervisor add constraint historicalsupervisor_fk2 foreign key (empid) references historical.employee(empid);
+alter table historical.supervisor add constraint historicalsupervisor_pk primary key (empid, supervisorid);
