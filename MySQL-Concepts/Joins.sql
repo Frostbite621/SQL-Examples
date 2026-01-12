391ae97c981276816f0f8fcbcc5b526f71718673
Frostbite621 <97651468+Frostbite621@users.noreply.github.com>
2026-01-11 23:02:22 -0600

    Update Joins.sql

--- MySQL-Concepts/Joins.sql
+++ MySQL-Concepts/Joins.sql
@@ -1,1 +1,11 @@
+SELECT e2.ename as "Executive"
+FROM employee e1
+join employee e2 on e1.supervisorid = e2.employeeid
+WHERE e1.ename = e2.ename;
+
+SELECT e2.ename, e1.ename
+FROM employee e1
+join employee e2 on e1.supervisorid = e2.employeeid
+ORDER BY e2.ename;
+
 
