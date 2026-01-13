e32c4515f6271b07b09e48d67d6415f885876c06
Frostbite621 <97651468+Frostbite621@users.noreply.github.com>
2026-01-12 19:57:18 -0600

    Update Employees_Earning_More_Than_Their_Managers.sql

--- leetcode-easy/Employees_Earning_More_Than_Their_Managers.sql
+++ leetcode-easy/Employees_Earning_More_Than_Their_Managers.sql
@@ -1,1 +1,8 @@
+--Write a solution to find employees 
+--who make more than their managers
+--Utilizes query and join
 
+SELECT e.name as "Employee"
+FROM employee e
+left join employee m on e.managerid = m.id
+WHERE e.salary > m.salary;
