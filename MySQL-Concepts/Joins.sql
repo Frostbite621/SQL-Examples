87a03bdaeed9afd8b987469292197dd0c8451d4c
Frostbite621 <97651468+Frostbite621@users.noreply.github.com>
2026-01-12 19:39:28 -0600

    Update Joins.sql

--- MySQL-Concepts/Joins.sql
+++ MySQL-Concepts/Joins.sql
@@ -1,3 +1,6 @@
+--demonstrate knowledge of joins
+--join two employee tables together, 1 for employee, 1 for supervisor
+
 SELECT e2.ename as "Executive"
 FROM employee e1
 join employee e2 on e1.supervisorid = e2.employeeid
