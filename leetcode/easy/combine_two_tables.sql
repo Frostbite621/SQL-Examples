264f1e144c49ccf62613842c4faae42e4b17115d
Frostbite621 <97651468+Frostbite621@users.noreply.github.com>
2026-01-12 19:55:59 -0600

    Delete leetcode directory

--- leetcode/easy/combine_two_tables.sql
+++
@@ -1,6 +0,0 @@
---Combine two tables
---Return the first name, last name and city, state of all persons in the table
---
-SELECT person.firstname, person.lastname, address.city, address.state
-FROM person
-left join address on person.personid = address.personid;
